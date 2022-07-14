1dd7c8f7b36e1e48ca2e89cb4ae5b22a4fbc45df
= <mayorlen.ortega@gmail.com>
2022-07-14 11:50:56 -0400

    puerto

--- Examen-Mod-6/index.js
+++ Examen-Mod-6/index.js
@@ -2,3 +2,8 @@ const fs = require("fs");
 const http = require("http")
 const url = require("url")
 const axios = require("axios");
+
+
+
+http.createServer(async (req, res) => {
+}).listen(8080, console.log("Server iniciado en puerto 8080")); 
